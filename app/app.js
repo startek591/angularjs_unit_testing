5eec7be6f2027e2d4ea8d9af772a9ac32f43effd
Jonathan Fambro <startek591@gmail.com>
2022-04-14 13:30:02 -0400

    Unit testing for Components

--- app/app.js
+++ app/app.js
@@ -32,4 +32,22 @@
       }
     };
   }]);
+  myApp.component('heroDetail', {
+    templateUrl:'heroDetail.html',
+    controller: function HeroDetailController() {
+      var ctrl = this;
+      ctrl.delete = function() {
+        ctrl.onDelete({hero: ctrl.hero});
+      };
+  
+      ctrl.update = function(prop, value) {
+        ctrl.onUpdate({hero: ctrl.hero, prop: prop, value: value});
+      };
+    },
+    bindings: {
+      hero: '<',
+      onDelete: '&',
+      onUpdate: '&'
+    }
+  });
 })();
--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -67,6 +67,34 @@
         expect(mock.alert.calls.count()).toEqual(2);
         expect(mock.alert.calls.mostRecent().args).toEqual(["more\ntwo\nthird"]);
       });
+    });
+    describe('HeroDetailController', function(){
+      var $componentController;
+      beforeEach(inject(function(_$componentController_) {
+        $componentController = _$componentController_;
+      }));
+
+      it('should call the `onDelete` binding, when deleting the hero', function() {
+        var onDeleteSpy = jasmine.createSpy('onDelete');
+        var bindings = {hero: {}, onDelete: onDeleteSpy};
+        var ctrl = $componentController('heroDetail', null, bindings);
+
+        ctrl.delete();
+        expect(onDeleteSpy).toHaveBeenCalledWith({hero: ctrl.hero});
+      });
+
+      it('should call the `onUpdate` binding, when updating a property', function() {
+        var onUpdateSpy = jasmine.createSpy('onUpdate');
+        var bindings = {hero: {}, onUpdate: onUpdateSpy};
+        var ctrl = $componentController('heroDetail', null, bindings);
+        
+        ctrl.update('foo', 'bar');
+        expect(onUpdateSpy).toHaveBeenCalledWith({
+          hero: ctrl.hero,
+          prop: 'foo',
+          value: 'bar'
+        });
+      });
     })
   });
 })();
